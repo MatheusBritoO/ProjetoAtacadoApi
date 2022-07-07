778eb6b8e2b9f181ac0e64889768f94b214b46ee
MatheusBritoO <matheusovelar77@gmail.com>
2022-07-07 16:55:54 -0400

    Boas Praticas em Relatorio

---
+++ Atacado.Service/Estoque/RelatorioService.cs
@@ -0,0 +1,89 @@
+﻿using Atacado.EF.Database;
+using Atacado.Poco.Estoque;
+using Atacado.Repository.Estoque;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atacado.Service.Estoque
+{
+    public  class RelatorioService
+    {
+        private AtacadoContext contexto;
+        private CategoriaRepository categoriaRepo;
+        private SubcategoriaRepository subcategoriaRepo;
+        private ProdutoRepository produtoRepo;
+    
+    
+        public RelatorioService()
+        {
+            this.contexto = new AtacadoContext();
+            this.categoriaRepo = new CategoriaRepository(this.contexto);
+            this.subcategoriaRepo = new SubcategoriaRepository(this.contexto);
+            this.produtoRepo = new ProdutoRepository(this.contexto);         
+        }
+    
+        public List<RelatorioPoco> CategoriaPorID(int idCategoria)
+        {
+            List<RelatorioPoco> pesquisa =
+                (from cats in this.contexto.Categorias
+                join subs in this.contexto.Subcategorias on cats.IdCategoria equals subs.IdCategoria
+                join prods in this.contexto.Produtos on subs.IdCategoria equals prods.IdCategoria
+                where cats.IdCategoria == idCategoria
+                select new RelatorioPoco() 
+                {
+                          IdCategoria =  cats.IdCategoria,  
+                          DescricaoCategoria = cats.DescricaoCategoria,
+                          IdSubcategoria = subs.IdSubcategoria,
+                          DescricaoSubcategoria = subs.DescricaoSubcategoria,
+                          IdProduto = prods.IdProduto,
+                          DescricaoProduto = prods.DescricaoProduto,
+                }).ToList();
+            return pesquisa;
+        }
+
+
+
+        public List<RelatorioPoco> SubcategoriaPorID(int idSubcategoria)
+        {
+            List<RelatorioPoco> pesquisa =
+                 (from subs in this.contexto.Subcategorias
+                  join cats in contexto.Categorias on subs.IdCategoria equals cats.IdCategoria
+                  join prods in contexto.Produtos on subs.IdSubcategoria equals prods.IdSubcategoria
+                  where subs.IdSubcategoria== idSubcategoria
+                   select new RelatorioPoco()
+                   {
+                        IdCategoria = cats.IdCategoria,
+                         DescricaoCategoria = cats.DescricaoCategoria,
+                         IdSubcategoria = subs.IdSubcategoria,
+                         DescricaoSubcategoria = subs.DescricaoSubcategoria,
+                         IdProduto = prods.IdProduto,
+                         DescricaoProduto = prods.DescricaoProduto,
+                   }).ToList();
+                        return pesquisa;
+        }
+
+
+        public List<RelatorioPoco> ProdutoPorID(int idProduto)
+        {
+            List<RelatorioPoco> pesquisa =
+                 (from prods in this.contexto.Produtos
+                  join cats in contexto.Categorias on prods.IdCategoria equals cats.IdCategoria
+                  join subs in contexto.Subcategorias on prods.IdSubcategoria equals subs.IdSubcategoria
+                  where prods.IdProduto == idProduto
+                  select new RelatorioPoco()
+                  {
+                      IdCategoria = cats.IdCategoria,
+                      DescricaoCategoria = cats.DescricaoCategoria,
+                      IdSubcategoria = subs.IdSubcategoria,
+                      DescricaoSubcategoria = subs.DescricaoSubcategoria,
+                      IdProduto = prods.IdProduto,
+                      DescricaoProduto = prods.DescricaoProduto,
+                  }).ToList();
+            return pesquisa;
+        }
+
+    }
+}
---
+++ AtacadoApi/Controllers/ReworkRelatorioController.cs
@@ -0,0 +1,69 @@
+﻿using Atacado.Poco.Estoque;
+using Atacado.Service.Estoque;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtacadoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReworkRelatorioController : ControllerBase
+    {
+        private RelatorioService service;
+
+        public ReworkRelatorioController() : base()
+        {
+            this.service = new RelatorioService();
+        }
+        
+        [HttpGet("Categoria/PorID/{idCat:int}")]
+        public ActionResult<List<RelatorioPoco>> GetRelatorioPorCategoria(int idCat)
+        {
+            try
+            {
+                List<RelatorioPoco> resposta = this.service.CategoriaPorID(idCat);
+                return Ok(resposta);
+
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("Subcategoria/PorID/{idSub:int}")]
+        public ActionResult<List<RelatorioPoco>> GetPorSubcategoria(int idSub)
+        {
+            try
+            {
+                List<RelatorioPoco> resposta = this.service.SubcategoriaPorID(idSub);
+                return Ok(resposta);
+
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("Produtos/PorID/{idPro:int}")]
+        public ActionResult<List<RelatorioPoco>> GetPorProduto(int idPro)
+        {
+            try
+            {
+                List<RelatorioPoco> resposta = this.service.ProdutoPorID(idPro);
+                return Ok(resposta);
+
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+
+
+
+    }
+}
